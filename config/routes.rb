fa75d66afec6adb679c16bc6cbbc959823a64c05
Jônatas Rancan de Souza <jonatasrancan@gmail.com>
2018-11-30 11:26:15 -0200

    Change labels on chart form and change show route to post

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :charts, except: [:new, :create, :edit, :update, :show, :destroy, :index] do
       collection do
         get :new
-        get '/show', action: :show
+        post '/show', action: :show
         get :resource_attributes
         get :data
       end
